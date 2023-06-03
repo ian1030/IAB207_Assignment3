27ee2cd470f84643b0d3f668b8433b3801b3f977
soozkai <132037218+soozkai@users.noreply.github.com>
2023-06-03 08:11:14 +0800

    Update views.py

--- Assignment3_template/website/views.py
+++ Assignment3_template/website/views.py
@@ -1,72 +1,87 @@
-from flask import Blueprint, render_template, request,  redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
 from .models import Event, Order, User
 from . import db
+from datetime import datetime
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    event = Event.query.all()  
-    return render_template('index.html', events=event)
+    events = Event.query.all()
+    current_datetime = datetime.now()
+    return render_template('index.html', events=events, current_datetime=current_datetime)
+
 
 @bp.route('/search')
 def search():
     if request.args['search']:
-        print(request.args['search'])
         search = '%' + request.args['search'] + '%'
-        events = Event.query.filter(Event.event_name.like(search)).all() or Event.query.filter(Event.event_category.like(search)).all()
+        events = Event.query.filter(Event.event_category.like(search)).all()
         return render_template('index.html', events=events)
     else:
         return redirect(url_for('main.index'))
+
 
 @bp.route('/all')
 def all():
     return redirect(url_for('main.index'))
 
+
 @bp.route('/run')
 def run():
     search = '%' + 'run' + '%'
     events = Event.query.filter(Event.event_category.like(search)).all()
-    return render_template('index.html', events=events)
+    current_datetime = datetime.now()
+    return render_template('index.html', events=events, current_datetime=current_datetime)
 
 
 @bp.route('/auction')
 def auction():
     search = '%' + 'auction' + '%'
     events = Event.query.filter(Event.event_category.like(search)).all()
-    return render_template('index.html', events=events)
+    current_datetime = datetime.now()
+    return render_template('index.html', events=events, current_datetime=current_datetime)
+
 
 @bp.route('/donation')
 def donation():
     search = '%' + 'donation' + '%'
     events = Event.query.filter(Event.event_category.like(search)).all()
-    return render_template('index.html', events=events)
+    current_datetime = datetime.now()
+    return render_template('index.html', events=events, current_datetime=current_datetime)
+
 
 @bp.route('/other')
 def other():
     search = '%' + 'other' + '%'
     events = Event.query.filter(Event.event_category.like(search)).all()
-    return render_template('index.html', events=events)
-    
+    current_datetime = datetime.now()
+    return render_template('index.html', events=events, current_datetime=current_datetime)
+
 
 @bp.route('/history')
 @login_required
 def history():
-    user_id = current_user.id  # Uncomment this line to assign the user_id variable
+    user_id = current_user.id
     order = Order.query.filter(Order.user_id.like(user_id)).all()
+    current_datetime = datetime.now()
     return render_template('history.html', orders=order)
+
 
 @bp.route('/searchOrder')
 def search_order():
     if request.args['search_order']:
-        print(request.args['search_order'])
         search = '%' + request.args['search_order'] + '%'
-        user_id = current_user.id  # Uncomment this line to assign the user_id variable
-        order = db.session.query(Order).join(Event).filter(Order.user_id == user_id, Event.event_name.like(search)).all()
-        return render_template('history.html', orders=order)       
+        user_id = current_user.id
+        order = (
+            db.session.query(Order)
+            .join(Event)
+            .filter(Order.user_id == user_id, Event.event_name.like(search))
+            .all()
+        )
+        return render_template('history.html', orders=order)
     else:
         return redirect(url_for('main.history'))
-    
-    #Order.query.filter(Event.event_description.like(search)).all() or Order.query.filter(Event.event_category.like(search)).all() or
+        return redirect(url_for('main.history'))
